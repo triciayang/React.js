be5333df3d12aa55ee9fb37c7fc692fe49bf4d83
양정연 <triciayang@iocentre.biz>
2023-02-08 16:36:17 +0900

    LifeCycle 접목 테스트

--- hello-react/src/views/Home.js
+++ hello-react/src/views/Home.js
@@ -1,4 +1,20 @@
+import React, {useState, useEffect} from 'react';
+
 function Home() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  function test() {
+    setCount(count + 1);
+    console.log('Event Fired!', count);
+  }
+  // componentDidMount, componentDidUpdate와 같은 방식으로
+  useEffect(() => {
+    // 브라우저 API를 이용하여 문서 타이틀을 업데이트합니다.
+    console.log('4444', count);
+    document.title = `You clicked ${count} times`;
+  }, [count]);
+
   return (
     <div className="app-home">
       <h1>Home</h1>
@@ -6,10 +22,6 @@ function Home() {
       <button onClick={test}>클릭 이벤트</button>
     </div>
   );
-}
-
-function test() {
-  console.log('Event Fired!');
 }
 
 export default Home;
